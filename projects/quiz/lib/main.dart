cad4e0f2c822a6e9150d3ecbc24bdee769b3d651
Eliúde Vemba <sweeteliude@gmail.com>
2023-02-19 23:17:33 +0100

    Finish Quiz App ✅

--- projects/quiz/lib/main.dart
+++ projects/quiz/lib/main.dart
@@ -1,66 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/widgets/quiz.dart';
+import 'package:quiz/widgets/result.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  var _selectedQuestion = 0;
+  var _total = 0;
+
+  final List<Map<String, Object>> questions = const [
+    {
+      'text': 'What is yours favourite color ?',
+      'answers': [
+        {'text': 'Black', 'value': 5},
+        {'text': 'White', 'value': 1},
+        {'text': 'Red', 'value': 0},
+        {'text': 'Blue', 'value': 3},
+      ],
+    },
+    {
+      'text': 'What is yours favourite animal ?',
+      'answers': [
+        {'text': 'Dog', 'value': 4},
+        {'text': 'Lion', 'value': 1},
+        {'text': 'Panther', 'value': 3},
+        {'text': 'Tiger', 'value': 2},
+      ],
+    },
+    {
+      'text': 'What is yours favourite programming language ?',
+      'answers': [
+        {'text': 'Python', 'value': 5},
+        {'text': 'C', 'value': 4},
+        {'text': 'Rust', 'value': 3},
+        {'text': 'SQL', 'value': 2},
+      ],
+    },
+    {
+      'text': 'What is yours favourite framework',
+      'answers': [
+        {'text': 'Adonis JS', 'value': 4},
+        {'text': 'Laravel', 'value': 1},
+        {'text': 'Flutter', 'value': 4},
+        {'text': 'React', 'value': 5},
+      ],
+    }
+  ];
+
+  void _responder(int value) {
+    setState(() {
+      _selectedQuestion++;
+      _total += value;
+    });
+
+    // print('Respondido: $_selectedQuestion');
+  }
+
+  void _reset() {
+    setState(() {
+      _selectedQuestion = 0;
+      _total = 0;
+    });
+  }
+
+  bool get hasSelectedQuestion {
+    return _selectedQuestion < questions.length;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz 2023'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        body: hasSelectedQuestion
+            ? Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Quiz(
+                  questions: questions,
+                  selectedQuestion: _selectedQuestion,
+                  responder: _responder,
+                ),
+              )
+            : Result(
+                score: _total,
+                reset: _reset,
+              ),
       ),
     );
   }
