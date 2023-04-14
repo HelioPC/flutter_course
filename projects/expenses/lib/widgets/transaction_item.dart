267d3aa9e55b3ab49d196e868ad8fdb0d5291ece
Eliúde Vemba <sweeteliude@gmail.com>
2023-04-14 21:15:09 +0100

    Fix android layout

--- projects/expenses/lib/widgets/transaction_item.dart
+++ projects/expenses/lib/widgets/transaction_item.dart
@@ -35,31 +35,58 @@ class _TransactionItemState extends State<TransactionItem> {
 
   void _removeTransaction() {
     HapticFeedback.vibrate();
-    showCupertinoModalPopup(
-      context: context,
-      builder: (context) {
-        return CupertinoActionSheet(
-          title: const Text('Delete'),
-          message: Text('Transaction \'${widget.tr.title}\' will be deleted'),
-          actions: [
-            CupertinoActionSheetAction(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancel'),
-            ),
-            CupertinoActionSheetAction(
-              isDestructiveAction: true,
-              onPressed: () {
-                widget.removeTransaction(widget.tr.id);
-                Navigator.pop(context);
-              },
-              child: const Text('Confirm'),
-            ),
-          ],
-        );
-      },
-    );
+    Platform.isIOS
+        ? showCupertinoModalPopup(
+            context: context,
+            builder: (context) {
+              return CupertinoActionSheet(
+                title: const Text('Delete'),
+                message:
+                    Text('Transaction \'${widget.tr.title}\' will be deleted'),
+                actions: [
+                  CupertinoActionSheetAction(
+                    isDestructiveAction: true,
+                    onPressed: () {
+                      widget.removeTransaction(widget.tr.id);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Confirm'),
+                  ),
+                  CupertinoActionSheetAction(
+                    isDefaultAction: true,
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              );
+            },
+          )
+        : showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Delete'),
+                content: Text(
+                  'Transaction \'${widget.tr.title}\' will be deleted',
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      widget.removeTransaction(widget.tr.id);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Confirm'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ],
+              );
+            });
   }
 
   @override
@@ -147,65 +174,3 @@ class _TransactionItemState extends State<TransactionItem> {
     );
   }
 }
-
-/*
-Slidable(
-                // Specify a key if the Slidable is dismissible.
-                key: const ValueKey(0),
-
-                // The start action pane is the one at the left or the top side.
-                startActionPane: ActionPane(
-                  // A motion is a widget used to control how the pane animates.
-                  motion: const ScrollMotion(),
-
-                  // A pane can dismiss the Slidable.
-                  dismissible: DismissiblePane(onDismissed: () {}),
-
-                  // All actions are defined in the children parameter.
-                  children: [
-                    // A SlidableAction can have an icon and/or a label.
-                    SlidableAction(
-                      onPressed: (BuildContext context) {},
-                      backgroundColor: const Color(0xFFFE4A49),
-                      foregroundColor: Colors.white,
-                      icon: Icons.delete,
-                      label: 'Delete',
-                    ),
-                    SlidableAction(
-                      onPressed: (BuildContext context) {},
-                      backgroundColor: const Color(0xFF21B7CA),
-                      foregroundColor: Colors.white,
-                      icon: Icons.share,
-                      label: 'Share',
-                    ),
-                  ],
-                ),
-
-                // The end action pane is the one at the right or the bottom side.
-                endActionPane: ActionPane(
-                  motion: const ScrollMotion(),
-                  children: [
-                    SlidableAction(
-                      // An action can be bigger than the others.
-                      flex: 2,
-                      onPressed: (BuildContext context) {},
-                      backgroundColor: const Color(0xFF7BC043),
-                      foregroundColor: Colors.white,
-                      icon: Icons.archive,
-                      label: 'Archive',
-                    ),
-                    SlidableAction(
-                      onPressed: (BuildContext context) {},
-                      backgroundColor: const Color(0xFF0392CF),
-                      foregroundColor: Colors.white,
-                      icon: Icons.save,
-                      label: 'Save',
-                    ),
-                  ],
-                ),
-
-                // The child of the Slidable is what the user sees when the
-                // component is not dragged.
-                child: const ListTile(title: Text('Slide me')),
-              )
-*/
