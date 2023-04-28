868dbce22a51acefc8c50fa47ed61f5b1f0d2863
Eliúde Vemba <sweeteliude@gmail.com>
2023-04-28 06:24:37 +0100

    FoodApp: replace tab bar navigation for bottom navbar

--- projects/food_app/lib/pages/tabs_page.dart
+++ projects/food_app/lib/pages/tabs_page.dart
@@ -2,35 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:food_app/pages/categories_page.dart';
 import 'package:food_app/pages/favorite_page.dart';
 
-class TabsPage extends StatelessWidget {
+class TabsPage extends StatefulWidget {
   const TabsPage({super.key});
 
   @override
+  State<TabsPage> createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  int _selectedIndex = 0;
+  final List<Map<String, Object>> _pages = [
+    {
+      'title': 'Categories',
+      'page': const CategoriesPage(),
+    },
+    {
+      'title': 'Favorites',
+      'page': const FavoritePage(),
+    },
+  ];
+
+  _changePage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Let\'s cook'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                text: 'Categories',
-                icon: Icon(Icons.category),
-              ),
-              Tab(
-                text: 'Favorites',
-                icon: Icon(Icons.favorite),
-              ),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            CategoriesPage(),
-            FavoritePage(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedIndex]['title'] as String),
+      ),
+      body: _pages[_selectedIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _changePage,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favorites'),
+        ],
       ),
     );
   }
