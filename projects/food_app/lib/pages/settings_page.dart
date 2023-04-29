e3c39b24d4e95803fde43979701af5d7363dcee8
Eliúde Vemba <sweeteliude@gmail.com>
2023-04-29 23:15:41 +0100

    FoodApp: add settings page

---
+++ projects/food_app/lib/pages/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting'),
+      ),
+      body: const Center(
+        child: Text('Settings page'),
+      ),
+    );
+  }
+}
--- projects/food_app/lib/utils/app_routes.dart
+++ projects/food_app/lib/utils/app_routes.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static const home = '/';
   static const categoriesMeals = '/categories-meals';
   static const mealDetail = '/meal-detail';
+  static const settings = '/settings';
 }
