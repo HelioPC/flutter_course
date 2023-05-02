08e1ddd621a81452655fd9765d80557a3b1134ef
Eliúde Vemba <sweeteliude@gmail.com>
2023-05-02 01:43:35 +0100

    Shop: add product item widget

---
+++ projects/shop/lib/pages/products_pverview_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  ProductsOverviewPage({super.key});
+
+  final List<Product> loadedProducts = dummyProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My shop'),
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 1.5,
+          ),
+          itemBuilder: (context, index) {
+            return ProductItem(product: loadedProducts[index]);
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ projects/shop/lib/widgets/product_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(overflow: TextOverflow.clip),
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
