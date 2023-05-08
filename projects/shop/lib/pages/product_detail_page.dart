c52a06f9229072fce48bc3cbe5c8367bc9a170b0
Eliúde Vemba <sweeteliude@gmail.com>
2023-05-09 00:58:00 +0100

    Shop: add detail page

--- projects/shop/lib/pages/product_detail_page.dart
+++ projects/shop/lib/pages/product_detail_page.dart
@@ -12,6 +12,37 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${product.price} \$',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
