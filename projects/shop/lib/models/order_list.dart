da92e6668f47e7c6bc8a9d214a01a68be1534813
Eliúde Vemba <sweeteliude@gmail.com>
2023-05-27 20:45:02 +0100

    Shop: save orders on firebase

--- projects/shop/lib/models/order_list.dart
+++ projects/shop/lib/models/order_list.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/constants/constants.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order.dart';
 
@@ -11,14 +13,36 @@ class OrderList with ChangeNotifier {
 
   int get itemsCount => _items.length;
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+
+    final response = await http.post(
+      Uri.parse('${Constants.ordersBaseUrl}.json'),
+      body: jsonEncode({
+        'total': cart.totalAmount,
+        'date': date.toIso8601String(),
+        'products': cart.items.values
+            .map(
+              (e) => {
+                'id': e.id,
+                'productId': e.productId,
+                'name': e.name,
+                'quantity': e.quantity,
+                'price': e.price,
+              },
+            )
+            .toList(),
+      }),
+    );
+
+    final id = jsonDecode(response.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         products: cart.items.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
     notifyListeners();
--- projects/shop/lib/pages/cart_page.dart
+++ projects/shop/lib/pages/cart_page.dart
@@ -43,21 +43,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      Provider.of<OrderList>(
-                        context,
-                        listen: false,
-                      ).addOrder(cart);
-                      cart.clear();
-                    },
-                    style: TextButton.styleFrom(
-                      textStyle: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    child: const Text('Comprar'),
-                  ),
+                  CartButton(cart: cart),
                 ],
               ),
             ),
@@ -75,6 +61,48 @@ class CartPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: _isLoading,
+      replacement: TextButton(
+        onPressed: widget.cart.itemsCount == 0
+            ? null
+            : () async {
+                setState(() => _isLoading = true);
+                await Provider.of<OrderList>(
+                  context,
+                  listen: false,
+                ).addOrder(widget.cart);
+                widget.cart.clear();
+                setState(() => _isLoading = false);
+              },
+        style: TextButton.styleFrom(
+          textStyle: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+        child: const Text('Comprar'),
+      ),
+      child: const CircularProgressIndicator(),
     );
   }
 }
