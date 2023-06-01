9c39e1e7cfc3417c12f0b52a38244a913ce297c3
Eliúde Vemba <sweeteliude@gmail.com>
2023-06-01 23:28:16 +0100

    Quiz app 🚀

--- projects/shop/lib/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const productBaseUrl =
-      'https://flutter-shop-6b598-default-rtdb.firebaseio.com/products';
-  static const ordersBaseUrl =
-      'https://flutter-shop-6b598-default-rtdb.firebaseio.com/orders';
-}
--- projects/shop/lib/utils/routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const auth = '/';
-  static const productDetail = '/product-detail';
-  static const cart = '/cart';
-  static const orders = '/orders';
-  static const products = '/products';
-  static const productForm = '/productForm';
-}
