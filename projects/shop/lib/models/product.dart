b7992a1d8205af125582c0c34f3024eddca91428
Eliúde Vemba <sweeteliude@gmail.com>
2023-06-01 23:12:29 +0100

    Shop: toggle favorite request with token

--- projects/shop/lib/models/product.dart
+++ projects/shop/lib/models/product.dart
@@ -26,12 +26,12 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavorite() async {
+  Future<void> toggleFavorite(String? token) async {
     try {
       _toggleFavorite();
 
       final response = await http.patch(
-        Uri.parse('${Constants.productBaseUrl}/$id.json'),
+        Uri.parse('${Constants.productBaseUrl}/$id.json?auth=$token'),
         body: jsonEncode({
           'isFavorite': isFavorite,
         }),
--- projects/shop/lib/widgets/product_grid_item.dart
+++ projects/shop/lib/widgets/product_grid_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/utils/routes.dart';
@@ -11,6 +12,7 @@ class ProductGridItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
+    final auth = Provider.of<Auth>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -21,7 +23,7 @@ class ProductGridItem extends StatelessWidget {
             builder: (ctx, value, child) {
               return IconButton(
                 onPressed: () {
-                  product.toggleFavorite();
+                  product.toggleFavorite(auth.token);
                 },
                 icon: Icon(
                   product.isFavorite ? Icons.favorite : Icons.favorite_border,
