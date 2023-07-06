379215856f8ac827002860c7643e677b5db9e6b4
Eliúde Vemba <sweeteliude@gmail.com>
2023-07-06 23:57:48 +0100

    Places: take picture with camera

--- projects/great_places/lib/widgets/image_input.dart
+++ projects/great_places/lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,20 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile? imageFile =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (imageFile != null) {
+      setState(() {
+        _storedImage = File(imageFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -22,12 +39,20 @@ class _ImageInputState extends State<ImageInput> {
             ),
           ),
           alignment: Alignment.center,
-          child: const Text('No image'),
+          child: Visibility(
+            visible: _storedImage != null,
+            replacement: const Text('No image'),
+            child: Image.file(
+              _storedImage!,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
         const SizedBox(height: 10),
         Expanded(
           child: TextButton.icon(
-            onPressed: () {},
+            onPressed: _takePicture,
             icon: const Icon(Icons.camera),
             label: const Text('Take a pick'),
           ),
