d5f274eea29e998cc3f095c806bb731367d12a79
Eliúde Vemba <sweeteliude@gmail.com>
2023-07-10 23:18:55 +0100

    Places: add place list

--- projects/great_places/lib/pages/places_list_page.dart
+++ projects/great_places/lib/pages/places_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListPage extends StatelessWidget {
   const PlacesListPage({super.key});
@@ -8,6 +10,7 @@ class PlacesListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
         centerTitle: true,
         title: const Text('My places'),
         actions: [
@@ -19,8 +22,41 @@ class PlacesListPage extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator.adaptive(),
+      body: Consumer<GreatPlaces>(
+        builder: (context, value, child) {
+          return Visibility(
+            visible: value.count > 0,
+            replacement: child!,
+            child: ListView.builder(
+              itemCount: value.count,
+              itemBuilder: (context, index) {
+                final place = value.list[index];
+
+                return ListTile(
+                  onTap: () async {
+                    await showModalBottomSheet(
+                      showDragHandle: true,
+                      context: context,
+                      builder: (context) {
+                        return const Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [SizedBox(width: double.infinity)],
+                        );
+                      },
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(place.image),
+                  ),
+                  title: Text(place.title),
+                );
+              },
+            ),
+          );
+        },
+        child: const Center(
+          child: Text('No places added'),
+        ),
       ),
     );
   }
--- projects/great_places/lib/widgets/image_input.dart
+++ projects/great_places/lib/widgets/image_input.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart' as syspath;
 
 class ImageInput extends StatefulWidget {
   final void Function(File) onSelectImage;
