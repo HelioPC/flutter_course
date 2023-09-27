df691e20180842b924daaba3ad20b44853ef0377
Eliúde Vemba <sweeteliude@gmail.com>
2023-09-27 11:13:08 +0100

    Chat: Add push notification (Android only)

--- projects/chat_app/lib/pages/auth_or_app_page.dart
+++ projects/chat_app/lib/pages/auth_or_app_page.dart
@@ -3,14 +3,20 @@ import 'package:chat_app/pages/auth_page.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/loading_page.dart';
 import 'package:chat_app/services/auth/auth_service_impl.dart';
+import 'package:chat_app/services/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+
+    if (context.mounted) {
+      await Provider.of<ChatNotificationService>(context, listen: false).init();
+    }
   }
 
   @override
--- projects/chat_app/lib/services/notification/chat_notification_service.dart
+++ projects/chat_app/lib/services/notification/chat_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/model/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -18,5 +19,50 @@ class ChatNotificationService with ChangeNotifier {
     _list.removeAt(i);
 
     notifyListeners();
+  }
+
+  // Push notification
+
+  Future<void> init() async {
+    await _configureTerminated();
+    await _configureForeground();
+    await _configureBackground();
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await _isAuthorized) {
+      RemoteMessage? initialMsg =
+          await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initialMsg);
+    }
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  void _messageHandler(RemoteMessage? event) {
+    if (event == null) return;
+    if (event.notification != null) {
+      add(ChatNotification(
+        title: event.notification?.title ?? 'Not informed',
+        body: event.notification?.body ?? 'Not informed',
+      ));
+    }
   }
 }
