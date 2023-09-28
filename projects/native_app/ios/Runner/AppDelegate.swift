cc440bb84c97002519de4cd9119e45f702f41b9d
Eliúde Vemba <sweeteliude@gmail.com>
2023-09-28 23:06:41 +0100

    Native: set ios (swift) communication

--- projects/native_app/ios/Runner/AppDelegate.swift
+++ projects/native_app/ios/Runner/AppDelegate.swift
@@ -7,6 +7,27 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let nativeChannel = FlutterMethodChannel(name: "native/sum", binaryMessenger: controller.binaryMessenger)
+      
+      nativeChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          
+          guard call.method == "sum" else {
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          
+          if let args = call.arguments as? [String: Any],
+             let num1 = args["num1"] as? Int,
+             let num2 = args["num2"] as? Int {
+              result(num1 + num2)
+          } else {
+              result(-1)
+          }
+      })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
